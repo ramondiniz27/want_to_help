4a40558b05a7111b8d8fd10131dbc5772cc99736
Ramon Diniz <ramon.diniz27@gmail.com>
2017-06-06 23:04:05 -0300

    seed dos estados criada

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cadastrando as ESTADOS..."
+states = [
+    "Acre - AC",
+    "Alagoas - AL",
+    "Amapá - AP",
+    "Amazonas - AM",
+    "Bahia - BA",
+    "Ceará - CE",
+    "Distrito Federal - DF",
+    "Espírito Santo - ES",
+    "Goiás - GO",
+    "Maranhão - MA",
+    "Mato Grosso - MT",
+    "Mato Grosso do Sul - MS",
+    "Minas Gerais - MG",
+    "Pará - PA",
+    "Paraíba - PB",
+    "Paraná - PR",
+    "Pernambuco - PE",
+    "Piauí - PI",
+    "Rio de Janeiro - RJ",
+    "Rio Grande do Norte - RN",
+    "Rio Grande do Sul - RS",
+    "Rondônia - RO",
+    "Roraima - RR",
+    "Santa Catarina - SC",
+    "São Paulo - SP",
+    "Sergipe - SE",
+    "Tocantins - TO",
+]
+states.each do |state|
+	State.find_or_create_by(name: states)
+end
+puts "ESTADOS cadastradas!"
